d6c162dad8a572add8201bc67b3aacc96603b285
Pedro-Firmani <132527747+Pedro-Firmani@users.noreply.github.com>
2024-05-23 19:20:36 -0300

    Add files via upload

---
+++ FilaSimples/src/Executor.java
@@ -0,0 +1,30 @@
+public class Executor {
+    public static void main(String[] args) {
+        FilaSimples fila = new FilaSimples(5);
+        fila.exibirFila();
+        fila.adicionarElemento(2);
+        fila.adicionarElemento(2);
+        fila.adicionarElemento(2);
+        fila.adicionarElemento(4);
+        fila.adicionarElemento(-2);
+        fila.exibirFila();
+        fila.removerElemento();
+        fila.adicionarElemento(8);
+        fila.exibirFila();
+        fila.buscarElemento(2);
+        fila.buscarElemento(8);
+        fila.buscarMenor();
+        fila.buscarMaior();
+        fila.media();
+        fila.quantidadeElementos(2);
+        fila.aumentarCapacidade(8);
+        fila.exibirFila();
+        fila.adicionarElemento(9);
+        fila.exibirFila();
+        fila.aumentarCapacidade(7);
+        fila.aumentarCapacidade(10);
+        fila.exibirFila();
+        fila.removerTodos();
+        fila.exibirFila();
+    }
+}
---
+++ FilaSimples/src/FilaSimples.java
@@ -0,0 +1,192 @@
+public class FilaSimples {
+    private int tamanho;
+    private Integer fila[];
+    private int novoTamanho;
+
+
+    public FilaSimples(int tamanho) {
+        this.tamanho = tamanho;
+        this.fila = new Integer[this.tamanho];
+        System.out.println("A fila foi criada");
+    }
+
+    public void exibirFila() {
+        for (int i = 0; i < this.tamanho; i++) {
+            System.out.println(fila[i]);
+        }
+    }
+
+    public boolean cheio() {
+        for (int i = 0; i < tamanho; i++) {
+            if (fila[i] == null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean vazio() {
+        for (int i = 0; i < tamanho; i++) {
+            if (fila[i] != null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void adicionarElemento(Integer elemento) {
+        if (cheio() == false) {
+            for (int i = 0; i < tamanho; i++) {
+                if (fila[i] == null) {
+                    fila[i] = elemento;
+                    System.out.println("O elemento foi adicionado");
+                    break;
+                }
+            }
+        } else {
+            System.out.println("A fila está cheia");
+        }
+    }
+
+    public void removerElemento() {
+        if (vazio() == false) {
+            int i = 0;
+            fila[i] = null;
+            for (i = 0; i <= tamanho - 2; i++) {
+                fila[i] = fila[i + 1];
+            }
+            fila[i] = null;
+            System.out.println("O elemento foi removido");
+        } else {
+            System.out.println("A fila não possui elementos");
+        }
+    }
+
+    public void removerTodos() {
+        if (vazio() == false) {
+            int i = 0;
+            do {
+                fila[i] = null;
+                i++;
+            } while (i != tamanho);
+            System.out.println("Todos os elementos foram removidos");
+
+        } else {
+            System.out.println("A fila não possui elementos");
+        }
+    }
+
+    public boolean verificarElemento(Integer elemento) {
+        int i = 0;
+        do {
+            if (fila[i] == elemento) {
+                return true;
+            }
+            i++;
+        } while (i != tamanho);
+        return false;
+    }
+
+    public void buscarElemento(Integer elemento) {
+        if (vazio() == false) {
+            if (verificarElemento(elemento) == true) {
+                int i = 0;
+                do {
+                    if (fila[i] == elemento) {
+                        System.out.println("O elemento " + elemento + " foi encontrado na fila no indice " + i);
+                    }
+                    i++;
+                } while (i != tamanho);
+            } else {
+                System.out.println("A fila não possui o elemento " + elemento);
+            }
+            verificarElemento(elemento);
+        } else {
+            System.out.println("A fila não possui elementos");
+        }
+    }
+
+    public void buscarMenor() {
+        if (vazio() == false) {
+            int i = 0;
+            int aux = fila[i];
+            do {
+                if (aux > fila[i]) {
+                    aux = fila[i];
+                }
+                i++;
+            } while (i != tamanho);
+            System.out.println("O menor elemento da fila é " + aux);
+        } else {
+            System.out.println("A fila não possui elementos");
+        }
+    }
+
+    public void buscarMaior() {
+        if (vazio() == false) {
+            int i = 0;
+            int aux = fila[i];
+            do {
+                if (aux < fila[i]) {
+                    aux = fila[i];
+                }
+                i++;
+            } while (i != tamanho);
+            System.out.println("O maior elemento da fila é " + aux);
+        } else {
+            System.out.println("A fila não possui elementos");
+        }
+    }
+
+    public void media() {
+        if (vazio() == false) {
+            int i = 0;
+            float soma = 0;
+            do {
+                soma += fila[i];
+                i++;
+            } while (i != tamanho);
+            System.out.println("A média dos elementos da fila é " + soma / tamanho);
+        } else {
+            System.out.println("A fila não possui elementos");
+        }
+    }
+
+    public void quantidadeElementos(Integer elemento) {
+        if (vazio() == false) {
+            if (verificarElemento(elemento) == true) {
+                int i = 0;
+                int contagem = 0;
+                do {
+                    if (elemento == fila[i]) {
+                        contagem += 1;
+                    }
+                    i++;
+                } while (i != tamanho);
+                System.out.println("A quantidade de elementos " + elemento + " é " + contagem);
+            } else {
+                System.out.println("A fila não possui o elemento " + elemento);
+            }
+        } else {
+            System.out.println("A fila não possui elementos");
+        }
+    }
+
+    public void aumentarCapacidade(int novoTamanho) {
+        Integer aux[] = fila;
+        if (novoTamanho > tamanho) {
+            Integer[] novaFila = new Integer[novoTamanho];
+            this.fila = novaFila;
+            int i = 0;
+            do {
+                fila[i] = aux[i];
+                i++;
+            } while (i != tamanho);
+            this.tamanho = novoTamanho;
+
+            System.out.println("A capacidade da fila foi aumentada");
+        } else {
+            System.out.println("O tamanho inserido é menor que o tamanho da fila original.");
+        }
+    }
+}
